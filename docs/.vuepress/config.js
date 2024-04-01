972503779cf8d607123a5366528f4712793fd422
1020325258 <1020325258@qq.com>
2024-04-01 22:36:08 +0800

    docs：删除一些无关属性

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -17,7 +17,6 @@ module.exports = {
 
     ['meta', { name: 'wwads-cn-verify', content: '6c4b761a28b734fe93831e3fb400ce87' }], // 广告相关，你可以去掉
     ['script', { src: 'https://cdn.wwads.cn/js/makemoney.js', type: 'text/javascript' }], // 广告相关，你可以去掉
-    ['script', { charset: 'utf-8', src: 'https://readmore.openwrite.cn/js/readmore.js' }]
   ],
 
   // 主题配置
